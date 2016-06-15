5ed155acc51b1b6e41ae28c316529eabffbbc8c9
Joseph Cota <jcota@getaroom.com>
2016-06-15 12:50:18 -0500

    Using a system similar to `twbs/bootstrap-rubygem` for loading gem stylesheet assets into livingstyleguide . Not sure about  compatibility ATM this is a rough cut for the first pass.

--- lib/livingstyleguide.rb
+++ lib/livingstyleguide.rb
@@ -16,29 +16,48 @@ module LivingStyleGuide
     footer: %Q(<div class="lsg--footer"><a class="lsg--logo" href="http://livingstyleguide.org">Made with the LivingStyleGuide gem.</a></div>),
     root: "/"
   }
+  class << self
+    def default_options
+      @@default_options
+    end
 
-  def self.default_options
-    @@default_options
-  end
+    def command(*keys, &block)
+      Commands.command(*keys, &block)
+    end
 
-  def self.command(*keys, &block)
-    Commands.command(*keys, &block)
-  end
-
-  def self.parse_data(data)
-    if data
-      if Psych.respond_to?(:safe_load)
-        Psych.safe_load(data)
-      else
-        Psych.load(data)
+    def parse_data(data)
+      if data
+        if Psych.respond_to?(:safe_load)
+          Psych.safe_load(data)
+        else
+          Psych.load(data)
+        end
       end
     end
-  end
 
-  def self.template(file, scope)
-    file = "#{File.dirname(__FILE__)}/livingstyleguide/templates/#{file}"
-    erb = File.read(file)
-    ERB.new(erb).result(scope)
+    def template(file, scope)
+      file = "#{File.dirname(__FILE__)}/livingstyleguide/templates/#{file}"
+      erb = File.read(file)
+      ERB.new(erb).result(scope)
+    end
+
+    def gem_path
+      @gem_path ||= File.expand_path '..', File.dirname(__FILE__)
+    end
+
+    def stylesheets_path
+      File.join assets_path, 'stylesheets'
+    end
+
+    def assets_path
+      @assets_path ||= File.join gem_path, 'assets'
+    end
+
+    def configure_sass
+      require 'sass'
+
+      ::Sass.load_paths << stylesheets_path
+    end
   end
 end
 
@@ -47,3 +66,5 @@ require "livingstyleguide/markdown_extensions"
 require "livingstyleguide/document"
 require "livingstyleguide/commands"
 require "livingstyleguide/integration"
+
+LivingStyleGuide.configure_sass
